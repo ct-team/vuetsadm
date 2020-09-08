e775ce5d2a9cd8fce1f8f60ee002c7602cbe11de
ct-team <328581342@qq.com>
2020-09-08 15:34:24 +0800

    Delete breadcrumb.vue

--- template/src/pages/breadcrumb.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <el-breadcrumb separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">TypeScript</el-breadcrumb-item>
-    <el-breadcrumb-item>首页</el-breadcrumb-item>
-  </el-breadcrumb>
-</template>
